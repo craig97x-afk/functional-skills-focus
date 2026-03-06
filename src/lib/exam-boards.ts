a826a9bb25bb803740e5a461115036af9d181946
Craig White <craigdw1997@gmail.com>
2026-03-06 14:44:19 +0000

    Limit exam boards to Edexcel, City & Guilds, NCFE, and AQA

--- src/lib/exam-boards.ts
+++ src/lib/exam-boards.ts
@@ -10,7 +10,7 @@ const allLevels = ["entry-1", "entry-2", "entry-3", "fs-1", "fs-2"];
 export const examBoards: ExamBoard[] = [
   {
     slug: "pearson-edexcel",
-    name: "Pearson Edexcel",
+    name: "Edexcel",
     subjects: ["english", "maths"],
     levels: allLevels,
   },
@@ -29,78 +29,6 @@ export const examBoards: ExamBoard[] = [
   {
     slug: "aqa",
     name: "AQA",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "highfield",
-    name: "Highfield Qualifications",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "open-awards",
-    name: "Open Awards",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "nocn",
-    name: "NOCN",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "vtct-skills",
-    name: "VTCT Skills",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "tquk",
-    name: "TQUK",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "gateway-qualifications",
-    name: "Gateway Qualifications",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "futurequals",
-    name: "FutureQuals",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "ocn-london",
-    name: "OCN London",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "learning-machine",
-    name: "The Learning Machine",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "wjec-essential-skills-wales",
-    name: "WJEC (Essential Skills Wales)",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "agored-cymru",
-    name: "Agored Cymru (Essential Skills Wales)",
-    subjects: ["english", "maths"],
-    levels: allLevels,
-  },
-  {
-    slug: "ocn-ni",
-    name: "OCN NI (Essential Skills NI)",
     subjects: ["english", "maths"],
     levels: allLevels,
   },
